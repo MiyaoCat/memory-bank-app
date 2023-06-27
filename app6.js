d6622dfe0ceb6c9e4a50dc23fd05797aa2abf659
john <jjmiyao@gmail.com>
2023-06-26 23:24:53 -0700

    list functions practice

---
+++ app6.js
@@ -0,0 +1,9 @@
+let athleteList = [
+		{	
+			"id": "sea-01"
+			"athlete": "Dejaunte Murray",
+			"team": "Atlanta Hawks",
+			"college": "University of Washington",
+			"high school": "Rainier Beach"
+		}
+	]
